9c2782182a5eb07f8b328f70f421bd096f7144b6
Admin <Admin@DESKTOP-R7G672U>
2020-02-25 17:40:22 +0900

    asd

--- study04/src/main/java/com/study04/www/service/IAllDelete.java
+++
@@ -1,7 +0,0 @@
-package com.study04.www.service;
-
-import java.util.HashMap;
-
-public interface IAllDelete {
-	public void setAllDelete(HashMap<String, Object> param);
-}
--- study04/src/main/java/com/study04/www/service/ILogin.java
+++
@@ -1,7 +0,0 @@
-package com.study04.www.service;
-
-import java.util.HashMap;
-
-public interface ILogin<T1> {
-	public T1 getLogin(HashMap<String, Object>param);
-}
